bc1511bd72f93a7fb7484b48c31df32869b416d5
khan214hg <99759539+khan214hg@users.noreply.github.com>
2025-06-15 17:13:08 +0500

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import pandas as pd
+import os
+
+def load_admissions():
+    if os.path.exists("admissions.csv"):
+        return pd.read_csv("admissions.csv")
+    else:
+        return pd.DataFrame(columns=["Name", "Class", "Contact", "Status"])
+
+def load_students():
+    if os.path.exists("students.csv"):
+        return pd.read_csv("students.csv")
+    else:
+        return pd.DataFrame(columns=["Name", "Class", "Roll Number", "Contact"])
+
+def load_staff():
+    if os.path.exists("staff.csv"):
+        return pd.read_csv("staff.csv")
+    else:
+        return pd.DataFrame(columns=["Name", "Position", "Contact", "Status"])
 
