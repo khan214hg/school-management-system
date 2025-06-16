10d33831ef44389090cc4122bd53a9f3ec461dab
khan214hg <99759539+khan214hg@users.noreply.github.com>
2025-06-16 14:34:57 +0500

    firebase_init.py

--- firebase_init.py
+++ firebase_init.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 # Load service account (Streamlit secrets ya direct file ka path)
-cred = credentials.Certificate("path/to/your-service-account-key.json")
+cred = credentials.Certificate("serviceaccountkey.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://your-project-id.firebaseio.com'
 })
