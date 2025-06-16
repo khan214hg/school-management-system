146d193feaf0cad47507ec66af4fbb702b221a61
khan214hg <99759539+khan214hg@users.noreply.github.com>
2025-06-16 14:25:34 +0500

    firebase_init.py

--- firebase_init.py
+++ firebase_init.py
@@ -7,3 +7,11 @@ cred = credentials.Certificate("path/to/your-service-account-key.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://your-project-id.firebaseio.com'
 })
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate("serviceaccountkey.json")
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
