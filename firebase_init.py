979c2521c08b80ce2e9a610dc5767f66371e8387
khan214hg <99759539+khan214hg@users.noreply.github.com>
2025-06-16 14:40:52 +0500

    firebase_init.py

--- firebase_init.py
+++ firebase_init.py
@@ -4,7 +4,12 @@ from firebase_admin import credentials, firestore
 import json
 
 if not firebase_admin._apps:
-    firebase_config = json.loads(st.secrets["firebase_creds"])
+    firebase_config = json.loads(st.secrets["import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+"])
     cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
 
