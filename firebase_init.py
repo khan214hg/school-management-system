5a10a8923b9f72c6f0660b733e0155cff1ced53b
khan214hg <99759539+khan214hg@users.noreply.github.com>
2025-06-16 14:38:51 +0500

    firebase_init.py

--- firebase_init.py
+++ firebase_init.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore
 import json
 
-firebase_config = json.loads(st.secrets["firebase"])
-cred = credentials.Certificate(firebase_config)
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    firebase_config = json.loads(st.secrets["firebase_creds"])
+    cred = credentials.Certificate(firebase_config)
+    firebase_admin.initialize_app(cred)
+
 db = firestore.client()
